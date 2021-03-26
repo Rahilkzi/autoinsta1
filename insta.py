c629eb419642ed4cb99b4f2bbe7244fa50652482
Rahil <44476072+Rahilkaxi@users.noreply.github.com>
2021-03-27 01:00:38 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -9,6 +9,7 @@ print ("starting")
 
 
 def job():
+    print ("starting job")
     option = webdriver.ChromeOptions()
     option.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     option.add_argument('--headless')
@@ -20,38 +21,38 @@ def job():
     time.sleep(20)
 
     username = driver.find_element_by_id("username")
-    time.sleep(30)
+    time.sleep(10)
 
     username.send_keys("booksmok")
-    time.sleep(30)
+    time.sleep(15)
 
     submit = driver.find_element_by_id("gonder")
     submit.click()
-    time.sleep(30)
+    time.sleep(20)
 
     password = driver.find_element_by_id("password")
     password.send_keys("book01")
-    time.sleep(30)
+    time.sleep(10)
     submit = driver.find_element_by_id("gonder")
     submit.click()
-    time.sleep(30)
+    time.sleep(20)
 
     sendF = driver.find_element_by_xpath("/html/body/main/div/div[2]/div[1]/div/div[2]/a/div/button")
     sendF.click()
-    time.sleep(30)
+    time.sleep(20)
 
     username = driver.find_element_by_id("username")
     time.sleep(10)
     username.send_keys("rahil_kzi")
-    time.sleep(30)
+    time.sleep(15)
 
     submit = driver.find_element_by_id("gonder")
     submit.click()
-    time.sleep(30)
+    time.sleep(20)
 
     no = driver.find_element_by_id("adet")
     no.send_keys('30')
-    time.sleep(30)
+    time.sleep(15)
 
     submit = driver.find_element_by_id("gonder")
     submit.click()

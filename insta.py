a0bd03e46ac87c0ef4c483a334b7c80784d28f49
Rahil <44476072+Rahilkaxi@users.noreply.github.com>
2021-03-26 16:42:30 +0530

    Add files via upload

---
+++ insta.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+import schedule
+import time
+
+URL = "https://increasefollower.com/giriss"
+
+
+def job():
+    GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+    CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+    option = webdriver.ChromeOptions()
+    option.add_argument('headless')
+    option.binary_location = GOOGLE_CHROME_PATH
+    driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, options=option)
+    # driver = webdriver.Chrome('chromedriver', options=option)
+
+    driver.get(URL)
+
+    username = driver.find_element_by_id("username")
+
+    username.send_keys("bookmysmoky")
+    time.sleep(1)
+
+    submit = driver.find_element_by_id("gonder")
+    submit.click()
+    time.sleep(5)
+
+    password = driver.find_element_by_id("password")
+    password.send_keys("bookmyshow")
+    submit = driver.find_element_by_id("gonder")
+    submit.click()
+    time.sleep(10)
+
+    sendF = driver.find_element_by_xpath("/html/body/main/div/div[2]/div[1]/div/div[2]/a/div/button")
+    sendF.click()
+    time.sleep(5)
+
+    username = driver.find_element_by_id("username")
+    time.sleep(2)
+    username.send_keys("rahil_kzi")
+    time.sleep(4)
+
+    submit = driver.find_element_by_id("gonder")
+    submit.click()
+    time.sleep(5)
+
+    no = driver.find_element_by_id("adet")
+    no.send_keys('30')
+    time.sleep(2)
+
+    submit = driver.find_element_by_id("gonder")
+    submit.click()
+
+    driver.quit()
+
+
+# schedule.every().day.at("10:30").do(job)
+schedule.every(3).seconds.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)

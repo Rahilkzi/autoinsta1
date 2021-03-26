a26b1c832b64356250ce6401f3874fb99e649b14
Rahil <44476072+Rahilkaxi@users.noreply.github.com>
2021-03-26 18:27:36 +0530

    Add files via upload

--- insta.py
+++ insta.py
@@ -15,51 +15,51 @@ def job():
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=option)
 
     driver.get(URL)
-    time.sleep(5)
+    time.sleep(30)
 
     username = driver.find_element_by_id("username")
-    time.sleep(5)
+    time.sleep(30)
 
     username.send_keys("bookmysmoky")
-    time.sleep(5)
+    time.sleep(30)
 
     submit = driver.find_element_by_id("gonder")
     submit.click()
-    time.sleep(10)
+    time.sleep(30)
 
     password = driver.find_element_by_id("password")
     password.send_keys("bookmyshow")
-    time.sleep(5)
+    time.sleep(30)
     submit = driver.find_element_by_id("gonder")
     submit.click()
-    time.sleep(10)
+    time.sleep(30)
 
     sendF = driver.find_element_by_xpath("/html/body/main/div/div[2]/div[1]/div/div[2]/a/div/button")
     sendF.click()
-    time.sleep(10)
+    time.sleep(30)
 
     username = driver.find_element_by_id("username")
-    time.sleep(5)
+    time.sleep(30)
     username.send_keys("rahil_kzi")
-    time.sleep(5)
+    time.sleep(30)
 
     submit = driver.find_element_by_id("gonder")
     submit.click()
-    time.sleep(10)
+    time.sleep(30)
 
     no = driver.find_element_by_id("adet")
     no.send_keys('30')
-    time.sleep(5)
+    time.sleep(30)
 
     submit = driver.find_element_by_id("gonder")
     submit.click()
-    time.sleep(2)
+    time.sleep(30)
 
     driver.quit()
 
 
-schedule.every().day.at("10:30").do(job)
-# schedule.every(3).seconds.do(job)
+# schedule.every().day.at("10:30").do(job)
+schedule.every(3).seconds.do(job)
 
 while True:
     schedule.run_pending()

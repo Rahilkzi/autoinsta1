040234878ad3413a71517867d41c2fa1ba481396
Rahil <44476072+Rahilkaxi@users.noreply.github.com>
2021-03-29 19:20:18 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -78,8 +78,8 @@ def job():
 
 
 # schedule.every().day.at("10:30").do(job)
-schedule.every(60).seconds.do(job)
-# schedule.every(2).hours.do(job)
+# schedule.every(60).seconds.do(job)
+schedule.every(4).hours.do(job)
 
 while True:
     schedule.run_pending()

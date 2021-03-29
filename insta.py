08f0632dc52417bedcb3e71c4993282d83eee827
Rahil <44476072+Rahilkaxi@users.noreply.github.com>
2021-03-29 19:06:44 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -5,66 +5,81 @@ import os
 
 URL = "https://increasefollower.com/giriss"
 
-option = webdriver.ChromeOptions()
-option.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-option.add_argument('--headless')
-option.add_argument("--no-sandbox")
-option.add_argument("--disable-dev-sh-usage")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=option)
-
-driver.get(URL)
-time.sleep(20)
-
-username = driver.find_element_by_id("username")
-time.sleep(5)
-username.send_keys("booksmok")
-time.sleep(10)
-submit = driver.find_element_by_id("gonder")
-submit.click()
-time.sleep(30)
-
-password = driver.find_element_by_id("password")
-time.sleep(5)
-password.send_keys("book01")
-time.sleep(5)
-submit = driver.find_element_by_id("gonder")
-submit.click()
-time.sleep(30)
-
-sendF = driver.find_element_by_xpath("/html/body/main/div/div[2]/div[1]/div/div[2]/a/div/button")
-time.sleep(5)
-sendF.click()
-time.sleep(30)
+print ("starting")
 
 
 def job():
-    user = driver.find_element_by_id("username")
-    time.sleep(5)
-    user.send_keys("rahil_kzi")
+    print ("starting job")
+    option = webdriver.ChromeOptions()
+    option.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    option.add_argument('--headless')
+    option.add_argument("--no-sandbox")
+    option.add_argument("--disable-dev-sh-usage")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=option)
+
+    print ("Getting Url")
+    driver.get(URL)
+    time.sleep(20)
+     
+
+    print ("Trying To Login")
+    print ("Typing Username")
+    username = driver.find_element_by_id("username")
     time.sleep(10)
-    submit_user = driver.find_element_by_id("gonder")
-    submit_user.click()
-    time.sleep(30)
-
-    amount = driver.find_element_by_id("adet")
-    time.sleep(5)
-    amount.send_keys('30')
+    username.send_keys("bookmysmoky")
+    time.sleep(15)
+    
+    print ("Submit Username")
+    submit = driver.find_element_by_id("gonder")
+    submit.click()
+    time.sleep(20)
+ 
+    print ("Typing Password")
+    password = driver.find_element_by_id("password")
+    password.send_keys("bookmyrool")
     time.sleep(10)
-    submit_amount = driver.find_element_by_id("gonder")
-    submit_amount.click()
+
+    print ("Submit Password")
+    submit = driver.find_element_by_id("gonder")
+    submit.click()
+    time.sleep(20)
+
+    
+    print ("Going to send followers")
+    sendF = driver.find_element_by_xpath("/html/body/main/div/div[2]/div[1]/div/div[2]/a/div/button")
+    sendF.click()
+    time.sleep(20)
+ 
+    print ("Typing Username for sending")
+    username = driver.find_element_by_id("username")
+    time.sleep(10)
+    username.send_keys("rahil_kzi")
+    time.sleep(15)
+ 
+    print ("Submit send followers")
+    submit = driver.find_element_by_id("gonder")
+    submit.click()
+    time.sleep(20)
+    
+    print ("How many follower")
+    no = driver.find_element_by_id("adet")
+    no.send_keys('30')
+    time.sleep(15)
+
+    print ("submit")
+    submit = driver.find_element_by_id("gonder")
+    submit.click()
     time.sleep(10)
 
+    print ("done")
+    time.sleep(2)
 
-def refresh():
-    driver.refresh()
-
-
-schedule.every(5).seconds.do(refresh)  # for refresh
+    driver.quit()
 
 
-schedule.every(3).hours.do(job)
 # schedule.every().day.at("10:30").do(job)
-# schedule.every(3).seconds.do(job)
+schedule.every(60).seconds.do(job)
+# schedule.every(2).hours.do(job)
 
 while True:
     schedule.run_pending()
